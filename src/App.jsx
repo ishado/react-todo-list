31625efd8a16da75831f80f53cfdd47254392312
Shadi Abdelslam <shadodev@gmail.com>
2023-12-20 22:27:45 +0200

    Create Todo Application
    - Delete any file not use
    - Create styles.css file
    - Create html elements for Todo app in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './styles.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className='new-item-form'>
+        <div className='form-row'>
+          <label htmlFor='item'>Todo Application</label>
+          <input
+            type="text"
+            id='item'
+          />
+        </div>
+        <button className="btn">add</button>
+      </form>
+      <h1 className='header'>Our Todo List</h1>
+      <ul className='list'>
+        <li>
+          <label>
+            <input type="checkbox" />
+            item 1
+          </label>
+          <button className='btn btn-danger'>Delete</button>
+        </li>
+      </ul>
     </>
   )
 }
 
-export default App
+export default App;
